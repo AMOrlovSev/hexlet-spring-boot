757d80928f7e17b382e7a8fc8859d8712fb91ef6
AMOrlovSev <amorlov.sev@gmail.com>
2026-09-19 23:13:15 +0300

    WelcomeController # @Value("${app.welcome-message}")

--- src/main/java/io/hexlet/spring_boot/Application.java
+++ src/main/java/io/hexlet/spring_boot/Application.java
@@ -1,13 +1,10 @@
 package io.hexlet.spring_boot;
 
-import io.hexlet.spring_boot.model.Post;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @RestController
