0b33a4eab33c2c07a5bd6165e2516d7df4b09674
AMOrlovSev <amorlov.sev@gmail.com>
2026-09-25 15:39:08 +0300

    MethodArgumentNotValidException

--- src/main/java/io/hexlet/spring_boot/exception/GlobalExceptionHandler.java
+++ src/main/java/io/hexlet/spring_boot/exception/GlobalExceptionHandler.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -15,11 +18,13 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException ex) {
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body(
-                        "Validation failed: "
-                                + ex.getBindingResult().getFieldError().getDefaultMessage());
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.unprocessableEntity().body(errors);
     }
 
     @ExceptionHandler(Exception.class)
