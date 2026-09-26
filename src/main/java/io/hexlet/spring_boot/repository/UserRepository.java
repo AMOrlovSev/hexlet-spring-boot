1073c92121192ad33b3e6b7408a3dbb565f53b3d
AMOrlovSev <amorlov.sev@gmail.com>
2026-09-26 15:53:11 +0300

    SpringBoot CLEAR

--- src/main/java/io/hexlet/spring_boot/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package io.hexlet.spring_boot.repository;
-
-import io.hexlet.spring_boot.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
